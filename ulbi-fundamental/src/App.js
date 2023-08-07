140133c58906bd61dfa202c4a555917b7d4a013c
dmitriy3751 <fdmitrij2009@yandex.ru>
2023-08-08 01:59:33 +0300

    commit

---
+++ ulbi-fundamental/src/App.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
+
+function App() {
+    const [value, setValue] = useState('Текст в инпуте')
+
+
+  return (
+    <div className="App">
+      <ClassCounter/>
+    </div>
+  );
+}
+
+export default App;
---
+++ ulbi-fundamental/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom'
+import App from './App';
+
+// структура создана под старую 17-тую версию Реакта
+// в 18том она задаётся по-другому!
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
+
+
