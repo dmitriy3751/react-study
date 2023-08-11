68d08e724b425a91dd98e02b67edced54694dbdf
fadeevdmy3751 <fadeevdmy7991@gmail.com>
2023-08-11 18:14:12 +0300

    продолжение

--- ulbi-fundamental/src/App.js
+++ ulbi-fundamental/src/App.js
@@ -6,6 +6,7 @@ import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
 import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
 
 function App() {
     const [posts, setPosts] = useState([
@@ -13,15 +14,41 @@ function App() {
         {id: 2, title: 'Javascript 2', body: 'Description'},
         {id: 3, title: 'Javascript 3', body: 'Description'}
     ])
+    const [selectedSort, setSelectedSort] = useState('')
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
     }
 
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id))
+    }
+
+    const sortPosts = (sort) => {
+        setSelectedSort(sort)
+        setPosts([...posts].sort((a, b) =>
+            a[sort].localeCompare(b[sort])
+        ))
+    }
+
     return (
         <div className="App">
             <PostForm create={createPost}/>
-            <PostList posts={posts} title={"Посты про JS"}/>
+            <hr style={{margin: '15px 0'}}/>
+            <MySelect
+                value={selectedSort}
+                onChange={sortPosts}
+                defaultValue={"Сортировка"}
+                options={[
+                    {value: 'title', name: 'По названию'},
+                    {value: 'body', name: 'По описанию'}
+                ]}
+             />
+            {
+                posts.length !== 0
+                    ? <PostList remove={removePost} posts={posts} title={"Посты про JS"}/>
+                    : <h1 style={{textAlign: 'center'}}>Посты не найдены!</h1>
+            }
         </div>
       );
 }
