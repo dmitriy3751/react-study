e5cffe20b3e0357c27dc24366c328230036cc506
fadeevdmy3751 <fadeevdmy7991@gmail.com>
2023-08-30 17:46:48 +0300

    остановился на 1:24:35

--- ulbi-fundamental/src/App.js
+++ ulbi-fundamental/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useMemo, useState} from 'react';
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import './styles/App.css'
@@ -7,6 +7,7 @@ import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
 import PostForm from "./components/PostForm";
 import MySelect from "./components/UI/select/MySelect";
+import PostFilter from "./components/PostFilter";
 
 function App() {
     const [posts, setPosts] = useState([
@@ -14,7 +15,21 @@ function App() {
         {id: 2, title: 'Javascript 2', body: 'Description'},
         {id: 3, title: 'Javascript 3', body: 'Description'}
     ])
-    const [selectedSort, setSelectedSort] = useState('')
+    const [filter, setFilter] = useState({
+        sort: '',
+        searchQuery: ''
+    })
+
+    const sortedPosts = useMemo(() => {
+        if(filter.sort) {
+            return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
+        }
+        return posts
+    }, [filter.sort, posts])
+
+    const sortedAndSearchedPosts = useMemo(() => {
+        return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.searchQuery.toLowerCase()))
+    }, [filter.searchQuery, sortedPosts])
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
@@ -24,31 +39,12 @@ function App() {
         setPosts(posts.filter(p => p.id !== post.id))
     }
 
-    const sortPosts = (sort) => {
-        setSelectedSort(sort)
-        setPosts([...posts].sort((a, b) =>
-            a[sort].localeCompare(b[sort])
-        ))
-    }
-
     return (
         <div className="App">
             <PostForm create={createPost}/>
             <hr style={{margin: '15px 0'}}/>
-            <MySelect
-                value={selectedSort}
-                onChange={sortPosts}
-                defaultValue={"Сортировка"}
-                options={[
-                    {value: 'title', name: 'По названию'},
-                    {value: 'body', name: 'По описанию'}
-                ]}
-             />
-            {
-                posts.length !== 0
-                    ? <PostList remove={removePost} posts={posts} title={"Посты про JS"}/>
-                    : <h1 style={{textAlign: 'center'}}>Посты не найдены!</h1>
-            }
+            <PostFilter filter={filter} setFilter={setFilter} />
+            <PostList remove={removePost} posts={sortedAndSearchedPosts} title={"Посты про JS"}/>
         </div>
       );
 }
--- ulbi-fundamental/src/components/PostList.jsx
+++ ulbi-fundamental/src/components/PostList.jsx
@@ -2,7 +2,14 @@ import React, {useState} from 'react';
 import PostItem from "./PostItem";
 
 const PostList = ({posts, title, remove}) => {
-
+    if(!posts.length) {
+        return (
+            <h1 style={{textAlign: 'center'}}>
+                Посты не найдены!
+            </h1>
+        )
+    }
+    
     return (
         <div>
             <h1 style={{textAlign: "center"}}>
