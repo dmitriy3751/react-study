0a00276a303c69790e0ebe064b4e14d56ecb9b35
fadeevdmy3751 <fadeevdmy7991@gmail.com>
2023-09-04 19:15:28 +0300

    добавил

---
+++ red-full-redux-rtk-query/src/store/store.js
@@ -0,0 +1,21 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { createLogger } from 'redux-logger'
+import { api } from './api/api'
+import { reducer as favoritesReducer } from './favorites/favorites.slice'
+import { userSlice } from './user/user.slice'
+
+const logger = createLogger({
+	collapsed: true,
+})
+
+const reducers = combineReducers({
+	favorites: favoritesReducer,
+	user: userSlice.reducer,
+	[api.reducerPath]: api.reducer,
+})
+
+export const store = configureStore({
+	reducer: reducers,
+	middleware: getDefaultMiddleware =>
+		getDefaultMiddleware().concat(api.middleware).concat(logger),
+})
